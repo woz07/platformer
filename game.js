f6d82a2b3bcb082902ea020817412c3716e3d341
woz07 <muhsameer9@gmail.com>
2025-02-15 02:50:57 +0000

    todo: bot

--- game.js
+++ game.js
@@ -2,30 +2,47 @@ const engine = require('raylib')
 
 const width = 800
 const height = 450
-engine.InitWindow(width, height, "raylib [core] example - basic window")
+engine.InitWindow(width, height, "Platformer")
 engine.SetTargetFPS(60)
-var player_x = width / 2
-var player_y = height / 2
-var player_speed = 5
+
+var player = {
+    x: width / 2,
+    y: height / 2,
+    radius: 25,
+    speed: 5
+}
+
+var bot = {
+    x: 0,
+    y: 0,
+    radius: 15,
+    speed: 3
+}
 
 while (!engine.WindowShouldClose()) {
     engine.BeginDrawing()
 
     engine.ClearBackground(engine.WHITE)
-    engine.DrawCircle(player_x, player_y, 25, engine.BLACK);
-    if (engine.IsKeyDown(0x57)  && player_y != 25) {
-        player_y = player_y - player_speed
+    engine.DrawCircle(player.x, player.y, player.radius, engine.BLACK);
+    // draw bot
+    // <-- here
+
+    if (engine.IsKeyDown(0x57)  && player.y != 25) {
+        player.y -= player.speed
     }
-    if (engine.IsKeyDown(0x53)  && player_y != height - 25) {
-        player_y = player_y + player_speed
+    if (engine.IsKeyDown(0x53)  && player.y != height - 25) {
+        player.y += player.speed
     }
-    if (engine.IsKeyDown(0x41)  && player_x != 25) {
-        player_x = player_x - player_speed
+    if (engine.IsKeyDown(0x41)  && player.x != 25) {
+        player.x -= player.speed
     }
-    if (engine.IsKeyDown(0x44)  && player_x != width - 25) {
-        player_x = player_x + player_speed
+    if (engine.IsKeyDown(0x44)  && player.x != width - 25) {
+        player.y += player.speed
     }
     
+    // add bot logic, for bot to move x and y.
+    // make it, so that the bot moves towards the player no matter what.
+    // you can do this using player.x and player.y, using bot.x and bot.y
 
     engine.EndDrawing()
 }
