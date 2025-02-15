edab3c6689cd62722f7e6c52a819a1c695ef84cc
Respinity1 <iqbalshoaib150@gmail.com>
2025-02-15 02:10:34 +0000

    fixed: background

--- game.js
+++ game.js
@@ -8,8 +8,9 @@ engine.SetTargetFPS(60)
 while (!engine.WindowShouldClose()) {
     engine.BeginDrawing()
 
+    engine.ClearBackground(engine.WHITE)
     engine.DrawCircle(width / 2, height / 2, 25, engine.BLACK);
-
+    
     engine.EndDrawing()
 }
 engine.CloseWindow()
