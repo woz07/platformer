6c32a78b23d40f4dfcd2bbe3dd9055aec24c9503
woz07 <muhsameer9@gmail.com>
2025-02-15 01:56:31 +0000

    add: basic game.js

---
+++ game.js
@@ -0,0 +1,14 @@
+const engine = require('raylib')
+
+const width = 800
+const height = 450
+engine.InitWindow(width, height, "raylib [core] example - basic window")
+engine.SetTargetFPS(60)
+
+while (!r.WindowShouldClose()) {
+    engine.BeginDrawing();
+    engine.ClearBackground(engine.RAYWHITE)
+    engine.DrawText("Congrats! You created your first node-raylib window!", 120, 200, 20, engine.LIGHTGRAY)
+    engine.EndDrawing()
+}
+engine.CloseWindow()
