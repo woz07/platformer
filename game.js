fc6b950e8af7d3b06ff59ff1df2ce6fa57eff6d6
Respinity1 <iqbalshoaib150@gmail.com>
2025-02-15 02:01:17 +0000

    fix: error

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ const height = 450
 engine.InitWindow(width, height, "raylib [core] example - basic window")
 engine.SetTargetFPS(60)
 
-while (!r.WindowShouldClose()) {
+while (!engine.WindowShouldClose()) {
     engine.BeginDrawing();
     engine.ClearBackground(engine.RAYWHITE)
     engine.DrawText("Congrats! You created your first node-raylib window!", 120, 200, 20, engine.LIGHTGRAY)
